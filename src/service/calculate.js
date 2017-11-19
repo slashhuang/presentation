77c540a411bda8fab8cff3b20b68f9f3471b1e71
slashhuang <627284482@qq.com>
2017-11-19 15:26:26 +0800

    add async examples

--- src/service/calculate.js
+++
@@ -1,6 +0,0 @@
-export const calculate = (a) => {
-    if (a > 1) {
-        return 1
-    };
-    return 3;
-}
--- src/service/calculate.test.js
+++
@@ -1,7 +0,0 @@
-import { calculate } from './calculate';
-test('return 1', () => {
-    expect(calculate(2)).toBe(1);
-});
-test('return 3', () => {
-    expect(calculate(0)).toBe(3);
-});
